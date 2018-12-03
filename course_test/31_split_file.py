9c4605362d2e53b8ccf1e8f59bdae1d1accf50d8
wuzongyue <11091422@bbktel.com>
2018-12-03 21:54:25 +0800

    optimize

--- course_test/31_split_file.py
+++ course_test/31_split_file.py
@@ -1,13 +1,15 @@
 # coding:utf-8
 
 import os
+import sys
+f_date = sys.argv[1]
 
 if __name__ == "__main__":
     i = 1
-    while i < 13:
-        file_name = 'url_20181128_' + str(i) + 'kw'
+    while i < 14:
+        file_name = 'url_{0}_'.format(f_date) + str(i) + 'kw'
         start_num = (i - 1)*10000000 + 1
         end_num = 10000000*i
-        cmd1 = "sed -n '{0},{1}p' url_20181128 >>{2}".format(start_num, end_num, file_name)
+        cmd1 = "sed -n '{0},{1}p' url_{2} >>{3}".format(start_num, end_num, f_date,file_name)
         os.system(cmd1)
         i += 1
